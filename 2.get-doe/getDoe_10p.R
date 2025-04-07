557ce1546eac852110c1268a087f7a90531061fe
Yansong Huang <yansong.huang@ifremer.com>
2025-04-07 18:12:26 +0200

    improved structure

--- 2.get-doe/getDoe_10p.R
+++ 2.get-doe/getDoe_10p.R
@@ -9,14 +9,25 @@ source("run_up/auxiliar.R")
 
 # 1. Parameter perturbation -----------------------------------------------
 
-
-
-  # Initial set of parameters
-  parametersData = read.csv(file = "2.get-doe/data_perturbation/K.csv",
-                            header = FALSE,
-                            col.names = c("parameter", "value", "scale"),
-                            sep = ",",
-                            stringsAsFactors = FALSE)
+  
+  # 文件夹路径
+  parameter_path <- "2.get-doe/initial_parameters"
+  
+  # 获取所有初始参数文件路径
+  csv_files <- list.files(path = parameter_path, pattern = "\\.csv$", full.names = TRUE)
+  
+  # 读取并合并，添加 source 列
+  parametersData <- do.call(rbind, lapply(csv_files, function(file) {
+    df <- read.csv(file,
+                   header = FALSE,
+                   col.names = c("parameter", "value", "scale"),
+                   sep = ",",
+                   stringsAsFactors = FALSE)
+    # 添加文件名作为新列（不含扩展名）
+    df$source <- tools::file_path_sans_ext(basename(file))
+    return(df)
+  }))
+  
   
   # Estimation of min and max (limits of parameter distribution)
   parametersData$percentage = rep(0.10, dim(parametersData)[1])
@@ -34,5 +45,5 @@ source("run_up/auxiliar.R")
   # Building the matrix with the design of experiments (doe)
   doe = random_sampling(par = parametersData, r = 3, levels = 8, grid.jump = 4/7) # CHECK IT
   
-  saveRDS(object = doe, file = "2.get-doe/doe/test_10p.rds")
+  saveRDS(object = doe, file = "2.get-doe/doe/test_complete_10p.rds")
 
