61403a1a3b58fdc28bb6713865e8b5ab061e5654
Yansong-Huang <yansong.huang@imbrsea.eu>
2025-07-03 11:46:08 +0200

    log scale colour

--- 5.elementary_effect/EE_shiny_app_heatmap.R
+++ 5.elementary_effect/EE_shiny_app_heatmap.R
@@ -28,23 +28,42 @@ EE_stats[, param_name := factor(param_name, levels = unique(param_name))]
 heat_dt <- copy(EE_stats)
 heat_dt[, value := mu_star]  # 可以改成 mu、sigma 等指标
 
+# 添加 tooltip 信息，避免 ggplotly 显示 object Object
+heat_dt[, tooltip := paste0(
+  "Species: ", species,
+  "<br>Parameter: ", param_name,
+  "<br>&mu;*: ", round(value, 2)
+)]
+
 # ---------- UI ----------
 ui <- fluidPage(
   titlePanel("Elementary Effects Heatmap (Biomass, μ*)"),
   fluidRow(
     column(12,
-           plotlyOutput("heatmap", height = "1500px")
+           plotlyOutput("heatmap", height = "1800px")
     )
   )
 )
 
 # ---------- Server ----------
 server <- function(input, output, session) {
+  # 自定义图例刻度断点，比如你想显示 0, 10, 100, 1000 这样几档
+  legend_breaks <- c(0, 10, 100, 1000, 10000, 100000, 500000)
+  
+  # 对应标签（如果用默认数字格式也可以不写）
+  legend_labels <- scales::label_number(accuracy = 1)(legend_breaks)
+  
   output$heatmap <- renderPlotly({
-    p <- ggplot(heat_dt, aes(x = species, y = param_name, fill = value)) +
+    p <- ggplot(heat_dt, aes(x = species, y = param_name, fill = value, text = tooltip)) +
       geom_tile(color = "white", width = 0.9, height = 0.9) +
-      # scale_fill_viridis(option = "C", na.value = "grey90") +
-      scale_fill_viridis(option = "C", trans = "log1p", na.value = "grey90")
+      scale_fill_viridis(
+        option = "C",
+        trans = "log1p",
+        na.value = "grey90",
+        name = expression(mu["*"]),
+        breaks = legend_breaks,
+        labels = legend_labels,
+      )+
       theme_minimal(base_size = 12) +
       theme(
         axis.text.x = element_text(angle = 45, hjust = 1),
@@ -54,7 +73,7 @@ server <- function(input, output, session) {
         axis.title = element_blank()
       )
     
-    ggplotly(p, tooltip = c("x", "y", "fill")) %>%
+    ggplotly(p, tooltip = "text") %>%
       layout(margin = list(l = 120, r = 20, b = 80, t = 30))
   })
 }
