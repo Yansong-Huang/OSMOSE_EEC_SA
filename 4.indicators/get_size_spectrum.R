520fe3bd507db1d99dd3ab24c520cfcdd094eada
Yansong-Huang <yansong.huang@imbrsea.eu>
2025-08-01 21:01:58 +0200

    LFI by species for all simulations

--- 4.indicators/get_size_spectrum.R
+++ 4.indicators/get_size_spectrum.R
@@ -13,7 +13,7 @@ source("4.indicators/indicators.R")
 ##  1. 路径 & 数据  ----
 ## ------------------------------------------------------------------
 input_dir   <- "morris_simulation_results"           # .rds 存放目录
-output_dir  <- "4.indicators/indicators_output/herring_perturbation"      # 指标/日志输出
+output_dir  <- "4.indicators/indicators_output"      # 指标/日志输出
 simulation_key <- fread("4.indicators/indicators_output/simulation_key.csv")
 dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 ## ------------------------------------------------------------------
@@ -31,56 +31,32 @@ simu_files <- simu_files[
   )
 ]
 
-
-## ------------------------------------------------------------------
-##  3. 找到所有变动了鲱鱼参数的模拟  ----
-## ------------------------------------------------------------------
-## 
-
-# 找到所有变动了鲱鱼参数的模拟，并选取当前模拟与前一模拟
-sel <- simulation_key[grepl("sp11", changed_param)]
-
-
-# 收集需要的 simulation_id：当前的 + 前一个
-sim_ids_to_load <- unique(c(sel$simulation_id, sel$simulation_id-1))
-# 筛选不合法编号
-sim_ids_to_load <- unique(c(sel$simulation_id, sel$simulation_id - 1))
-sim_ids_to_load <- sim_ids_to_load[sim_ids_to_load > 0 & sim_ids_to_load <= length(simu_files)]
-
-
 ## ------------------------------------------------------------------
 ##  4. 初始化指标 List  ----
 ## ------------------------------------------------------------------
-
-out_LFI_sp                <- vector("list", length(sim_ids_to_load))
-out_yield_by_size_sp      <- vector("list", length(sim_ids_to_load))
-
+n_sim <- length(simu_files)
+out_LFI_sp                <- vector("list", length(n_sim))
 
 ## ------------------------------------------------------------------
 ##  5. 主循环：读取 + 计算指标  ----
 ## ------------------------------------------------------------------
 
-for (i in seq_along(sim_ids_to_load)) { 
-  sim_id <- sim_ids_to_load[i]
-  cat("Processing sim_id:", sim_id, "file:", basename(simu_files[sim_id]), "\n")
-  
-  object <- readRDS(simu_files[sim_id])
+for (i in seq_len(n_sim)) { 
+  object <- readRDS(simu_files[i])
   
   yield          <- object$osmose.yield
   yield_by_size  <- object$osmose.yieldBySize
   
-  out_yield_by_size_sp[[i]]    <- .YieldBySize_to_df(yield_by_size)
   out_LFI_sp[[i]]              <- .LFI_by_species(yield, yield_by_size, thr = 40)
   
   ## 释放大对象
   rm(object, yield, yield_by_size)
-  if (i %% 100 == 0) gc()
+  if (i %% 1000 == 0) gc()
 }
 
 ## ------------------------------------------------------------------
 ##  6. 保存所有指标  ----
 ## ------------------------------------------------------------------
-saveRDS(out_LFI_sp            , file = file.path(output_dir, "lfi_sp.rds"))
-saveRDS(out_yield_by_size_sp  , file = file.path(output_dir, "out_yield_by_size_sp.rds"))
+saveRDS(out_LFI_sp, file = file.path(output_dir, "lfi_sp.rds"))
 
 
