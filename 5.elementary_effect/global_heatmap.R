94ed7a3d6cecc7b3cff030cfb5972e6101a6ca6c
Yansong-Huang <yansong.huang@imbrsea.eu>
2025-08-18 16:28:27 +0200

    global heatmap

---
+++ 5.elementary_effect/global_heatmap.R
@@ -0,0 +1,69 @@
+rm(list = ls())
+
+library(data.table)
+library(pheatmap)
+library(viridis)
+library(stringr)
+library(dendextend)
+library(dplyr)
+library(gridExtra)
+
+
+# ----------准备----------
+#物种映射
+sp_names <- c(
+  "sp0" = "SYC",  "sp1" = "MUR",  "sp2" = "BIB",  "sp3" = "WHG",
+  "sp4" = "POD",  "sp5" = "COD",  "sp6" = "LYY",  "sp7" = "SOL",
+  "sp8" = "PLE",  "sp9" = "HOM",  "sp10" = "MAC", "sp11" = "HER",
+  "sp12" = "PIL", "sp13" = "SQZ", "sp14" = "CTC", "sp15" = "RJC"
+)
+# 指标
+indicators <- c("biomass", "mean_length", "LFI", "yield", "mean_TL")
+
+for(ind in indicators)
+{
+  # ---------- 读取数据 ----------
+  EE_stats <- fread(paste0("5.elementary_effect/EE_outputs/EE_",ind,"_by_species_stats.csv"))
+  
+  # non-exploited species 
+  if(ind %in% c("LFI", "yield"))
+    EE_stats <- filter(EE_stats, !(species %in% c("sp4", "sp6")))
+  
+  # ---------- 合并 param_type 和 param_label ----------
+  mapping <- fread("5.elementary_effect/param_name_map.csv")
+  EE_stats <- merge(EE_stats, mapping, by = "param_name", all.x = TRUE)
+  
+  
+  # 设定物种顺序
+  species_all <- paste0("sp", 0:15)
+  EE_stats[, species := factor(species, levels = species_all)]
+  
+  
+  # ---------- 构建 heatmap 数据矩阵 ----------
+  heat_mat <- dcast(EE_stats, param_label ~ species, value.var = "mu_star")
+  heat_mat <- as.data.frame(heat_mat)
+  rownames(heat_mat) <- heat_mat$param_label
+  heat_mat <- as.matrix(heat_mat[, -1])
+  
+  # 替换列名为物种缩写
+  colnames(heat_mat) <- sp_names[colnames(heat_mat)]
+  
+  heat_mat_log <- log1p(heat_mat)
+  
+  # ---------- 主热图 ----------
+  pheatmap(
+    mat = heat_mat_log,
+    cluster_rows = FALSE,   # 不聚类
+    cluster_cols = FALSE,   # 不聚类
+    color = viridis(100),
+    fontsize_row = 6,
+    fontsize_col = 10,
+    border_color = NA,
+    angle_col = 45,
+    main = paste("Heatmap of μ* (log1p) -", ind),
+    filename = paste0("figures/heatmap/heatmap_",ind,"_mu_star.png"),
+    width = 10,
+    height = 12
+  )
+  
+}
